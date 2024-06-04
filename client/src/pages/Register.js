4988cfd2eeca3f5285f7a80253241e9fd6385cec
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-06-04 16:36:27 +0530

    basic registration form created

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Button, Form, Input } from "antd";
 
 const Register = () => {
+  const onFinishHandler = (values) => {
+    console.log(values);
+  }
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className='form-container'>
+      <h1>Register form</h1>
+      <Form layout="vertical" onFinish={onFinishHandler}>
+      <Form.Item label="Name" name="name">
+      <Input type="text" required/>
+      </Form.Item>
+      <Form.Item label="Email" name="email">
+      <Input type="email" required/>
+      </Form.Item>
+      <Form.Item label="Password" name="password">
+      <Input type="password" required/>
+      </Form.Item>
+      <Button className='btn btn-primary' type='submit'>
+        Register
+      </Button>
+      </Form>
+      </div>
+    </>
   )
 }
 
