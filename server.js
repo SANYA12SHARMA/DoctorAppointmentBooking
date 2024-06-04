a52af3fa2597dfdd971f98821c1c877460dc7e33
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-06-04 21:22:39 +0530

    database connection done

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
-
+const connectDB = require('./config/db');
 dotenv.config();
 
 const app = express();
+
+
 
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
 
+//mongodb connection
+connectDB();
 app.get('/',(req,res)=>{
     res.status(200).send({
         message:"server running",
