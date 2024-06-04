76ec26fb8bd0f4ee5ec78735636576b6b5b325f4
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-06-04 21:43:08 +0530

    login controller logic written

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,3 +1,23 @@
+const userModel = require("../models/userModels");
+const bcrypt = require('bcryptjs');
+
+const registerController = async (req,res) => {
+    try{
+        const existingUser = await userModel.findOne({email:req.body.email});
+        if(existingUser) return res.status(200).send({success:true,message:"User already exist"});
+        const password = req.body.password;
+        const salt = bcrypt.genSalt(10);
+        const hashedPassword = bcrypt.hash(password,salt);
+        req.body.password = hashedPassword;
+        const newUser = new userModel(req.body);
+        await newUser.save();
+        return res.status(200).send({success:true,message:"User Registered Successfully"});
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).send({success:false,message:`Register Controller ${error.message}`});
+    }
+}
 const loginController = () => {}
-const registerController = () => {}
+
 module.exports = {loginController,registerController};
