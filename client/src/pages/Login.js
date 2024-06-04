8cbf4ca0496010f5258e7561d1f2b24fa41c2715
Sanya Sharma <sanyaasharma3241@gmail.com>
2024-06-04 17:54:24 +0530

    added concurrently dependency and login form page created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import { Form, Input } from "antd";
+import { Link } from 'react-router-dom';
+import "../styles/RegisterStyles.css";
 const Login = () => {
+  const onFinishHandler = (values) => {
+    console.log(values);
+  }
   return (
-    <div>
-      <h1>Login</h1>
+    <>
+    <div className='form-container'>
+      <Form layout="vertical" onFinish={onFinishHandler} className='register-form'>
+        <h3 className='text-center'>Login form</h3>
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className='m-2'>Not a user Register here.</Link>
+        <button className='btn btn-primary' type='submit'>
+          Login
+        </button>
+      </Form>
     </div>
+  </>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Button, Form, Input } from "antd";
+import React from 'react';
+import { Form, Input } from "antd";
+import { Link } from 'react-router-dom';
+import "../styles/RegisterStyles.css";
 
 const Register = () => {
   const onFinishHandler = (values) => {
     console.log(values);
   }
+
   return (
     <>
       <div className='form-container'>
-      <h1>Register form</h1>
-      <Form layout="vertical" onFinish={onFinishHandler}>
-      <Form.Item label="Name" name="name">
-      <Input type="text" required/>
-      </Form.Item>
-      <Form.Item label="Email" name="email">
-      <Input type="email" required/>
-      </Form.Item>
-      <Form.Item label="Password" name="password">
-      <Input type="password" required/>
-      </Form.Item>
-      <Button className='btn btn-primary' type='submit'>
-        Register
-      </Button>
-      </Form>
+        <Form layout="vertical" onFinish={onFinishHandler} className='register-form'>
+          <h3 className='text-center'>Register form</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+          <Link to="/login" className='m-2'>Already user login here.</Link>
+          <button className='btn btn-primary' type='submit'>
+            Register
+          </button>
+        </Form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
